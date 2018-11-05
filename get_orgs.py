f1f672c7debe878188a4536b99da9d718ff99d3b
Phillip Rowland <rowland.phillip@gmail.com>
2018-11-04 19:15:44 -0500

    Make better command line parameters

--- get_orgs.py
+++ get_orgs.py
@@ -15,52 +15,72 @@ B = '\033[94m'  # blue
 P = '\033[95m'  # purple
 Y = '\033[93m'  # yellow
 
-def main(apikey, username, debug=False):
+def main(username, actions, admin):
+
+    debug = 'd' in actions
+    if debug:
+        print(P+"Debugging Enabled"+W)
+    verbose = 'v' in actions
 
     # TODO: Build Errors Array for exception reporting
     # TODO: Build Debug Logger method to keep console clean
 
     m_orgs = []
 
+    # Accept Invitations, Enable API and do all Org related actions
+    if 'l' in actions or 't' in actions or 'b' in actions:
+        org_lics = process_orgs(username, actions)
+        if debug:
+            print("Return Value: " + str(org_lics))
+
+    apikey = keyring.get_password('merakiapi', username)
     orgs = meraki.myorgaccess(apikey, (not debug))
-    if debug: print(orgs)
+    if debug:
+        print(orgs)
 
-
-    for org in orgs:
-
-        # TODO: Can I dynamically add to the org dictionary of each element
-        print(G+"Checking Administrators in " + org['name'] + ":"+W)
-        grant_org_admin(apikey, org['id'], 'meraki_vpn@netsmartai.com', "NetSmart API", debug)
-
+    # Add administrators if requested
+    if 'a' in actions:
+        for org in orgs:
+            # TODO: Can I dynamically add to the org dictionary of each element
+            print(B+"Checking Administrators in " + org['name'] + ":"+W)
+            grant_org_admin(apikey, org['id'], admin, debug, verbose)
+            # TODO: Make this call dynamic based on the parameters
 
 
         # m_org = m_organization(org['id'], org['name'])
         # m_orgs.append(m_org)
 
 
-    org_lics = get_org_lics(username, debug)
-    if debug:
-        print("Return Value: " + str(org_lics))
 
-def grant_org_admin(apikey, orgid, email, name, debug=False):
+
+def grant_org_admin(apikey, orgid, new_admin, debug, verbose):
     org_admins = meraki.getorgadmins(apikey, orgid, (not debug))
     if debug:
         print(org_admins)
     try:
         notadmin = True
         for admin in org_admins:
-            print(G+"  " + admin['name'] + " has " + admin['orgAccess'] + " access"+W)
-            if admin['email'] == email:
+            if verbose:
+                print(G + "  " + admin['name'] + " has " + admin['orgAccess'] + " access" + W)
+            if admin['email'] == new_admin[0]:
                 notadmin = False
 
         if (notadmin):
-            print(Y+"  "+name + " not found, adding..."+W)
-            meraki.addadmin(apikey, orgid, email, name, 'full')
+            if verbose:
+                print(Y + "  " + new_admin[2] + " not found, adding..." + W)
+            resp = meraki.addadmin(apikey, orgid, new_admin[0], new_admin[2] , new_admin[1])
+            if debug:
+                print(resp)
     except TypeError:
-        print(R+"  Error processing this Organization"+W)
+        print(R + "  Error processing this Organization" + W)
+    except (KeyboardInterrupt, SystemExit):
+        raise
 
 
-def get_org_lics(username, debug=False):
+def process_orgs(username, actions):
+
+    debug = ('d' in actions)
+    verbose = ('v' in actions)
 
     org_ids = [] # [org_id, org_name, org_url]
     adv_lics = [] # [org_id, org_name, adv_license]
@@ -78,9 +98,11 @@ def get_org_lics(username, debug=False):
         # Get the Organization List, this is the default redirect page after login
         host = 'https://n94.meraki.com'
         p = s.post(host + '/login/login', data=payload)
-        if debug: print("Cookies: " + str(s.cookies))
+        if debug:
+            print("Cookies: " + str(s.cookies))
         soup = BeautifulSoup(p.text, 'html.parser')
-        if debug: print("URL: " + str(soup.url))
+        if debug:
+            print("URL: " + str(soup.url))
         
         # Parse out all Organization IDs, Names, and initiation URLs
         for item in soup.find_all('li'):
@@ -89,120 +111,249 @@ def get_org_lics(username, debug=False):
             org_id = org_a.get('href').split('=', 1)[1]
             org_url = org_a.get('href')
             org_ids.append([org_id, org_name, org_url])
-            if (debug): print("Org A tag: " + str(org_a))
+            if debug:
+                print("Org A tag: " + str(org_a))
+
+        # Follow first Org Redirect and check for new confirmations
+        print(G+"Checking for new Organization Access"+W)
+        org_redirect = s.get(host + '/login/org_choose?eid=' + (org_ids[0])[0])
+        if debug:
+            print(org_redirect.url)
+        soup = BeautifulSoup(org_redirect.content, 'html.parser')
+        forms = soup.find_all("form")
+
+        # Iterate through all new Organizations and accept access
+        for conf in forms:
+            # Get the form post url we need for this new Organization
+            post_url = conf.get('action')
+            if post_url.split('/')[4] == 'confirm_account_submit':
+                # Build form payload
+                token = conf.find('', {"name": "authenticity_token"}).get('value')
+                user_conf = conf.find('', {"name": "user_conf_key"}).get('value')
+                payload = {
+                    'utf8': "%E2%9C%93",
+                    'authenticity_token': token,
+                    'user_conf_key': user_conf,
+                    'commit': "Yes"
+                }
+
+                if debug:
+                    print("Found form: " + post_url + " - " + token + " - " + user_conf)
+
+                # Send Form Post to click the "Yes" button accepting access
+                if verbose:
+                    print(G+"  Accepting Org Access: " + uconf + W)
+                response_post = s.post(post_url, data=payload)
+                if debug:
+                    print(str(response_post))
+
+                # Set the host to the currently active server since we've issued another post
+                urlsplit = org_redirect.url.split('/')
+                host = urlsplit[0] + "//" + urlsplit[2]
+
+
 
         # Follow the Org Redirect, then open and parse the license details
         for org in org_ids:
-            print(G+"Accessing " + org[1] +W)
-            org_redirect = s.get(host + '/login/org_choose?eid=' + org[0])
-
-            # Break apart the redirected url and rebuild it for the license page
-            # TODO: Modularize this split since it will probably come up regularly
-            print(G+"  Querying Advanced License"+W)
-            urlsplit = org_redirect.url.split('/')
-            if (debug): print("URL Split: " + str(urlsplit))
-            url = urlsplit[0] + '//' + urlsplit[2] + '/o/' + org[0] + '/manage/dashboard/license_info'
-            org_license = s.get(url)
-            if (debug): print("New Org URL: " + str(org_license.url))
-
-            # Write out the HTML response if debugging so we can review for refactoring
-            if (debug):
-                handle = open(org[1] + '-get_adv_lics.html', "w")
-                handle.write(str(org_license.content))
-                handle.close()
-
-            # Parse the license table out by table row and store Advanced Security for return
+            print("Processing " + org[1])
             try:
-                soup = BeautifulSoup(org_license.content, 'html.parser')
-                license = soup.find("", {"id": "license_info_table"})
-                trs = license.find_all('tr')
-                if (debug): print(p)
-                for l in trs:
-                    if l.find('', {"class": "cfgq"}).text == "MX Advanced Security":
-                        # TODO: Parse out Expiration date and store for future use
-                        adv_lics.append([org[0], org[1], l.find('', {"class": "cfgs"}).text])
-            except:
-                print(R+"  Error Retrieving Advanced License"+W)
+                org_redirect = s.get(host + '/login/org_choose?eid=' + org[0])
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except Exception as e:
+                print(R + " Error accessing Organization" + W)
+                print(str(e))
+
 
             # Enable API access
-            print(G+"  Querying API Access"+W)
-            try:
-                url = urlsplit[0] + '//' + urlsplit[2] + '/o/' + org[0] + '/manage/organization/edit'
-                org_manage = s.get(url)
-                soup = BeautifulSoup(org_manage.content, 'html.parser')
-                api_status = soup.find('', {"id": "organization_provisioning_api_enabled"}).get('checked')
-                if str(api_status) == 'None':
-                    print(Y+"  Enabling API Access"+W)
-                    url = urlsplit[0] + '//' + urlsplit[2] + '/o/' + org[0] + '/manage/organization'
-                    token = soup.find('', {"name": "authenticity_token"}).get('value')
+            if 'b' in actions:
+                print("  Querying API Access")
+                try:
+                    urlsplit = org_redirect.url.split('/')
+                    url = urlsplit[0] + '//' + urlsplit[2] + '/o/' + org[0] + '/manage/organization/edit'
+                    org_manage = s.get(url)
+                    soup = BeautifulSoup(org_manage.content, 'html.parser')
+                    api_status = soup.find('', {"id": "organization_provisioning_api_enabled"}).get('checked')
+                    if str(api_status) == 'None':
+                        print(Y + "  Enabling API Access" + W)
+                        url = urlsplit[0] + '//' + urlsplit[2] + '/o/' + org[0] + '/manage/organization'
+                        token = soup.find('', {"name": "authenticity_token"}).get('value')
+                        if debug:
+                            print(token)
+                        payload = {
+                            'authenticity_token': token,
+                            'organization_provisioning_api_enabled': "1",
+                            'organization[provisioning_api_enabled]': "1"
+                        }
+                        response_post = s.post(url, data=payload)
+                        if debug:
+                            handle = open(org[1] + '-manage_org.html', "w")
+                            handle.write(str(response_post.content))
+                            handle.close()
+                except (KeyboardInterrupt, SystemExit):
+                    raise
+                except Exception as e:
+                    print(R + "  Error validating API Access" + W)
+                    print(str(e))
+
+
+            # Parse License Details if needed
+            if 'l' in actions or 't' in actions:
+                # Break apart the redirected url and rebuild it for the license page
+                # TODO: Modularize this split since it will probably come up regularly
+                print(G+"  Querying Advanced License"+W)
+                urlsplit = org_redirect.url.split('/')
+                if (debug): print("URL Split: " + str(urlsplit))
+                url = urlsplit[0] + '//' + urlsplit[2] + '/o/' + org[0] + '/manage/dashboard/license_info'
+                org_license = s.get(url)
+                if (debug): print("New Org URL: " + str(org_license.url))
+
+                # Write out the HTML response if debugging so we can review for refactoring
+                if (debug):
+                    handle = open(org[1] + '-get_adv_lics.html', "w")
+                    handle.write(str(org_license.content))
+                    handle.close()
+
+                # Parse the license table out by table row and store Advanced Security for return
+                try:
+                    soup = BeautifulSoup(org_license.content, 'html.parser')
+                    license = soup.find("", {"id": "license_info_table"})
+                    trs = license.find_all('tr')
                     if debug:
-                        print(token)
-                    payload = {
-                        'authenticity_token': token,
-                        'organization_provisioning_api_enabled': "1",
-                        'organization[provisioning_api_enabled]': "1"
-                    }
-                    response_post = s.post(url, data=payload)
-                    if debug:
-                        handle = open(org[1] + '-manage_org.html', "w")
-                        handle.write(str(response_post.content))
-                        handle.close()
-            except:
-                print(R+"  Error validating API Access"+W)
+                        print(p)
+                    for l in trs:
+                        if l.find('', {"class": "cfgq"}).text == "MX Advanced Security":
+                            # TODO: Parse out Expiration date and store for future use
+                            advanced_license = (l.find('', {"class": "cfgs"}).text == 'Enabled')
+                            adv_lics.append([org[0], org[1], advanced_license])
+                except (KeyboardInterrupt, SystemExit):
+                    raise
+                except:
+                    print(R+"  Error Retrieving Advanced License"+W)
+
+
+                if 't' in actions:
+                    # TODO: Parse all threat settings
+                    print('Threat analysis not implemented yet')
+
+            # TODO: If requested actions is theat gaps, get the details here
 
         return adv_lics
 
-""" Can't get details of License or Advanced Protection Settings here
-        orgNetworks = meraki.getnetworklist(apikey, org['id'])
-        for network in orgNetworks:
-            netDetail = meraki.getnetworkdetail(apikey, network['id'])
-            print(netDetail)
-"""
-
 def usage():
-    print('get_orgs.py -k <keyfile> -u <username>')
+    msg = 'get_orgs.py [-d] [-h] -e <login email> [-a <new admin email> -n <admin name>]'
+    msg += ' [-u <admin email>] [-p <new permission>] [-r <remove admin>] [-l] [-t] [-o] [-c]'
+    print(msg)
+    print(' d: Print Debugging messages')
+    print(' h: Print this usage message')
+    print(' e: The email/username authenticate with')
+    print(' a: Add this email to all organizations as admin')
+    print(' n: The name of the new admin')
+    print(' u: Update the permissions of this Admin')
+    print(' p: "full"|"read-only" for new/updated admin')
+    print(' r: Revoke rights from this Admin')
+    print(' l: Summarize Licenses')
+    print(' t: Print Threat gaps for security settings')
+    print(' o: The output file to write json results to')
+    print(' c: Limit json output file to a diff from this file')
+    print(' b: Validate API access')
+
 
 
 if __name__ == "__main__":
     keyfile = username = ''
+    admin = ['', 'read-only', '']
+    output_file = ''
+    debug = False
+    # TODO Convert admins to a dictionary
+
+    actions = []
 
     #   Get command line arguments and parse for options
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hk:u:')
+        opts, args = getopt.getopt(sys.argv[1:], 'dhe:a:n:u:p:r:lto:cvb', ['output-file='])
     except getopt.GetoptError as err:
         print(str(err))
         usage()
         sys.exit(2)
+
+    # print(opts)
+    # print(args)
+
     for opt, arg in opts:
-        if opt == '-h':
+        if opt == '-h': # Print usage menu
             usage()
             sys.exit()
-        elif opt == '-k':
-            keyfile = arg
-        elif opt == '-u':
+        elif opt == '-d': # Set debugging to on
+            actions.append('d')
+        elif opt == '-e': # Email Address to login with
             username = arg
+        elif opt == '-a': # Action mode: Add Admins
+            actions.append('a')
+            admin[0] = arg
+        elif opt == '-n': # Admin Name
+            admin[2] = arg
+        elif opt == '-u': # Action mode: Update Admins
+            actions.append('u')
+            admin[0] = arg
+        elif opt == '-p': # Permission to add/update for Admins
+            admin[1] = arg
+        elif opt == '-r': # Remove Admins
+            actions.append('r')
+            admin[0] = arg
+        elif opt == '-l': # List Licenses ( Summarize non-advanced at the end )
+            actions.append('l')
+        elif opt == '-t': # List Companies with their Threat Gaps
+            actions.append('t')
+        elif opt in ('-o', 'output-file='): # Output changes to file
+            output_file = arg
+            actions.append('o')
+        elif opt == '-c': # Output only changes to file
+            actions.append('c')
+        elif opt == '-v': # Process and print verbosely (slower)
+            actions.append('v')
+        elif opt == '-b': # Process API Access validation
+            actions.append('b')
+        # TODO: f for firewall rule gaps
+        # TODO: f arg as a json file to match (does firewall rules contain a rule with policy, protocol, destPort)
         else:
             assert False, "unhandled option"
 
-    #   Make sure we have a Key File option value
-    if keyfile == '':
-        print("the keyfile parameter is required")
-        usage()
-        sys.exit(2)
     #   Make sure we have a username
     if username == '':
         print("the username parameter is required")
         usage()
         sys.exit(2)
 
-    #   Open Specified Key File and read API Key into variable "apikey"
-    try:
-        keyhandle = open(keyfile, "r")
 
-        if keyhandle.mode == 'r':
-            apikey = keyhandle.read().rstrip()
-    except:
-        print("Error opening specified API Key File.")
-        sys.exit(2)
+    #   Make sure not more than one admin modification is selected
+    admin_count = 0
+    if 'a' in actions:
+        admin_count += 1
+        if admin[2] == '':
+            print('You must include a name with the -n parameter')
+            sys.exit()
+        #   Make sure we have a permission if we are to add an admin
+        # TODO Validate permission options
+    if 'u' in actions:
+        admin_count += 1
 
-    main(apikey, username)
+        #   Make sure we have a permission if we are to update an admin
+        # TODO Validate permission options
+        print('Update Admin feature not implemented yet')
+    if 'r' in actions:
+        admin_count += 1
+    if admin_count > 1:
+        print('Cannot select multiple administrator operations in one run')
+        sys.exit()
+
+    # ALL API prerequisites checked here
+    if admin_count == 1: # Or any other actions using API
+        #   Make sure we have an API Key stored
+        if str(keyring.get_password("merakiapi", username)) == 'None':
+            print("the username must have a 'merakiapi' key stored in keyring")
+            sys.exit(2)
+
+
+
+    main(username, actions, admin)
